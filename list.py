6be50bbb5d55a69543213417feddb869beda2f0b
Tomtubinas <143620957+Tomtubinas@users.noreply.github.com>
2024-04-17 09:19:01 -0400

    Update list.py
    Searches for items when adding

--- list.py
+++ list.py
@@ -11,7 +11,7 @@ def List():
         if item.lower() == "done":
             add_to_list = False
         else:
-            grocery_list.append(item.lower())
+            Locate(item)
 
     print("Your grocery list:")
     for item in grocery_list:
@@ -26,33 +26,37 @@ def Remove(grocery_list):
     else:
         print("Item not found in the list.")
 
-def Locate():
+def Locate(item):
+    search = []
     with open('doordashoutput.csv', 'r', encoding='latin-1') as file:
         for line in file:
             parts = line.strip().split(', ')
             name = parts[0]
             price = parts[1]
-            for item in grocery_list:
-                if item.lower() in name.lower():
-                    print(f"{name}, {price}")
-                    print("************************************")
-                    break
+            if item.lower() in name.lower():
+                search.append((name, price))
+    n = 1
+    for results in search:
+        print(f"{n}) {results[0]}, {results[1]}")
+        n += 1
+    add = input("What item do you want to add to your cart?")
+    add = int(add) - 1
+    print(search[int(add)])
+    addToList = search[add]
+    grocery_list.append(addToList)
 
 
 
 loop = True
 while loop:
-    print("Welcome to the program.\nWhat would you like to do?\n1) Make a list\n2) Remove an item\n3) Find items\n4) Exit")
+    print("Welcome to the program.\nWhat would you like to do?\n1) Make a list\n2) Remove an item\n3) Exit")
     option = input()
-
     if option == "1":
         List()
     elif option == "2":
         Remove(grocery_list)
     elif option == "3":
-        Locate()
-    elif option == "4":
         loop = False
         print("Exiting the program...")
     else:
-        print("Invalid option. Please enter 1, 2, 3, or 4.")
+        print("Invalid option. Please enter 1, 2, or 3")
